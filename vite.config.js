db7d24ebe84028de125a95a99b5d12646cabd9af
mrpejker <tatiana.yakushkina@gmail.com>
2024-02-17 21:22:36 +0300

    new page fixed 2

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        game: '/pages/game.html'
+      }
+    }
+  }
+});
